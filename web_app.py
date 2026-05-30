28a1c4cdf0a1c5c0f513287fcd3558430f5f13f5
zhan-zip <2899893413@qq.com>
2026-05-31 00:18:47 +0800

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -452,5 +452,5 @@ async def root():
     return HTMLResponse(content=html)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 7860))
     uvicorn.run(app, host="0.0.0.0", port=port)
